229711f05618d6741c38f119e8027026109aa673
Steven <stevensuncatalin369@gmail.com>
2020-09-05 09:30:01 +0300

    Tried something with the static files, now trying a new deploy

--- meshnet_project/settings.py
+++ meshnet_project/settings.py
@@ -127,9 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "meshnet_project/static")
+    os.path.join(BASE_DIR, "static")
 ]
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
